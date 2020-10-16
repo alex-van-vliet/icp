3af767a0b40215fa100cf391053652ef42d7f854
Théophane Vié <theophanevie@gmail.com>
2020-10-16 14:45:42 +0200

    functionnal tests: add basics

--- libcpu/libcpu/test_functionnal.cc
+++ libcpu/libcpu/test_functionnal.cc
@@ -7,22 +7,38 @@ namespace
 {
     using namespace libcpu;
 
+    void wrapper(const char* file1, const char* file2)
+    {
+        auto q = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
+        auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
+
+        auto [transform, new_p] = libcpu::icp(q, p, 200, 1e-5);
+
+        ASSERT_EQ(q == new_p, true);
+    }
+
     TEST(FunctionalTest, basic_translation)
     {
         auto file1 = "../data/line/line_ref.txt";
         auto file2 = "../data/line/line_translated_2_3_4.txt";
 
-        auto m = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
-        auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
+        wrapper(file1, file2);
+    }
 
-        auto transform = libcpu::find_alignment(p, m);
+    TEST(FunctionalTest, basic_rotation)
+    {
+        auto file1 = "../data/line/line_ref.txt";
+        auto file2 = "../data/line/line_rotated_90_135_180.txt";
 
-        auto result = utils::eye<float>(4);
-        result.set(0, 3, 2);
-        result.set(1, 3, 3);
-        result.set(2, 3, 4);
+        wrapper(file1, file2);
+    }
 
-        ASSERT_EQ(result == transform, true);
+    TEST(FunctionalTest, basic_scale)
+    {
+        auto file1 = "../data/line/line_ref.txt";
+        auto file2 = "../data/line/line_scaled_2_3_4.txt";
+
+        wrapper(file1, file2);
     }
 
 
