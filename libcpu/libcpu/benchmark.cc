281a99ceeac4dce2a76b860f15797524b19bf1e7
Théophane Vié <theophanevie@gmail.com>
2020-10-16 17:04:13 +0200

    bench: add cows and translation tests

--- libcpu/libcpu/benchmark.cc
+++ libcpu/libcpu/benchmark.cc
@@ -16,8 +16,40 @@ static void BM_CPU_cow2(benchmark::State& state)
     }
 }
 
+static void BM_CPU_cow1(benchmark::State& state)
+{
+    auto file1 = "../data/cow/cow_ref.txt";
+    auto file2 = "../data/cow/cow_tr1.txt";
+
+    auto q = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
+    auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
+    
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        auto [transform, new_p] = libcpu::icp(q, p, 200, 1e-5);
+    }
+}
+
+static void BM_CPU_line_translated(benchmark::State& state)
+{
+    auto file1 = "../data/line/line_ref.txt";
+    auto file2 = "../data/line/line_translated_2_3_4.txt";
+
+    auto q = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
+    auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
+    
+    // Perform setup here
+    for (auto _ : state) {
+        // This code gets timed
+        auto [transform, new_p] = libcpu::icp(q, p, 200, 1e-5);
+    }
+}
+
 // Register the function as a benchmark
 BENCHMARK(BM_CPU_cow2)->Unit(benchmark::kMillisecond)->UseRealTime();
+BENCHMARK(BM_CPU_cow1)->Unit(benchmark::kMillisecond)->UseRealTime();
+BENCHMARK(BM_CPU_line_translated)->Unit(benchmark::kMillisecond)->UseRealTime();
 // Run the benchmark
 BENCHMARK_MAIN();
 
