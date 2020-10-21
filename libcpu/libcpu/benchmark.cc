83b204cd28f0b165cfa779f75b8633513587e8e2
Alex van Vliet <alex.van-vliet@epita.fr>
2020-10-21 10:42:11 +0200

    bench: add horse

--- libcpu/libcpu/benchmark.cc
+++ libcpu/libcpu/benchmark.cc
@@ -1,4 +1,5 @@
 #include <benchmark/benchmark.h>
+
 #include "icp.hh"
 
 static void BM_CPU_cow2(benchmark::State& state)
@@ -8,9 +9,10 @@ static void BM_CPU_cow2(benchmark::State& state)
 
     auto q = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
     auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
-    
+
     // Perform setup here
-    for (auto _ : state) {
+    for (auto _ : state)
+    {
         // This code gets timed
         auto [transform, new_p] = libcpu::icp(q, p, 200, 1e-5);
     }
@@ -23,9 +25,42 @@ static void BM_CPU_cow1(benchmark::State& state)
 
     auto q = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
     auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
-    
+
     // Perform setup here
-    for (auto _ : state) {
+    for (auto _ : state)
+    {
+        // This code gets timed
+        auto [transform, new_p] = libcpu::icp(q, p, 200, 1e-5);
+    }
+}
+
+static void BM_CPU_horse1(benchmark::State& state)
+{
+    auto file1 = "../data/horse/horse_ref.txt";
+    auto file2 = "../data/horse/horse_tr1.txt";
+
+    auto q = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
+    auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
+
+    // Perform setup here
+    for (auto _ : state)
+    {
+        // This code gets timed
+        auto [transform, new_p] = libcpu::icp(q, p, 200, 1e-5);
+    }
+}
+
+static void BM_CPU_horse2(benchmark::State& state)
+{
+    auto file1 = "../data/horse/horse_ref.txt";
+    auto file2 = "../data/horse/horse_tr2.txt";
+
+    auto q = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
+    auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
+
+    // Perform setup here
+    for (auto _ : state)
+    {
         // This code gets timed
         auto [transform, new_p] = libcpu::icp(q, p, 200, 1e-5);
     }
@@ -38,9 +73,10 @@ static void BM_CPU_line_translated(benchmark::State& state)
 
     auto q = libcpu::read_csv(file1, "Points_0", "Points_1", "Points_2");
     auto p = libcpu::read_csv(file2, "Points_0", "Points_1", "Points_2");
-    
+
     // Perform setup here
-    for (auto _ : state) {
+    for (auto _ : state)
+    {
         // This code gets timed
         auto [transform, new_p] = libcpu::icp(q, p, 200, 1e-5);
     }
@@ -49,8 +85,8 @@ static void BM_CPU_line_translated(benchmark::State& state)
 // Register the function as a benchmark
 BENCHMARK(BM_CPU_cow2)->Unit(benchmark::kMillisecond)->UseRealTime();
 BENCHMARK(BM_CPU_cow1)->Unit(benchmark::kMillisecond)->UseRealTime();
+BENCHMARK(BM_CPU_horse2)->Unit(benchmark::kMillisecond)->UseRealTime();
+BENCHMARK(BM_CPU_horse1)->Unit(benchmark::kMillisecond)->UseRealTime();
 BENCHMARK(BM_CPU_line_translated)->Unit(benchmark::kMillisecond)->UseRealTime();
 // Run the benchmark
 BENCHMARK_MAIN();
-
-
